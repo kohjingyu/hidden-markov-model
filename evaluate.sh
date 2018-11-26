edb5fea4f8a30218cdf749e841e4fc7dad9f338d
Tze How <tzehow@traceto.io>
2018-11-26 17:12:37 +0800

    Changes to part 4 such that it runs ridiculously slowly now

--- evaluate.sh
+++ evaluate.sh
@@ -5,7 +5,7 @@ python3.6 evalResult.py data/EN/dev.out data/EN/dev.p2.out | grep F
 echo Part 3
 python3.6 evalResult.py data/EN/dev.out data/EN/dev.p3.out | grep F
 echo Part 4
-python3.6 evalResult.py data/EN/dev.out data/EN/dev.p4.out
+python3.6 evalResult.py data/EN/dev.out data/EN/dev.p4.out | grep F
 
 echo SG
 echo Part 2
@@ -25,4 +25,4 @@ python3.6 evalResult.py data/FR/dev.out data/FR/dev.p2.out | grep F
 echo Part 3
 python3.6 evalResult.py data/FR/dev.out data/FR/dev.p3.out | grep F
 echo Part 4
-python3.6 evalResult.py data/FR/dev.out data/FR/dev.p4.out
+python3.6 evalResult.py data/FR/dev.out data/FR/dev.p4.out | grep F
